ba87140fdb0bbda41e86b3bbd1287f058f55d9de
Niraj <bca210634_niraj@achsnepal.edu.np>
2023-06-20 19:26:55 +0545

    Added Some Functionality

--- js/script.js
+++ js/script.js
@@ -62,25 +62,3 @@ const activeElementOnScroll = function () {
 }
 
 window.addEventListener("scroll", activeElementOnScroll);
-
-
-
-/**
- * SCROLL REVEAL
- */
-
-// const revealElements = document.querySelectorAll("[data-reveal]");
-
-// const revealElementOnScroll = function () {
-//   for (let i = 0, len = revealElements.length; i < len; i++) {
-//     if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
-//       revealElements[i].classList.add("revealed");
-//     } else {
-//       revealElements[i].classList.remove("revealed");
-//     }
-//   }
-// }
-
-window.addEventListener("scroll", revealElementOnScroll);
-
-window.addEventListener("load", revealElementOnScroll);
