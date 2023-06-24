853d5c945663f6c9b4c1eb5323a6dac7743ee9f5
Niraj <bca210634_niraj@achsnepal.edu.np>
2023-06-24 18:57:49 +0545

    Added new php file to add Doctor in the Datbase

---
+++ php/addDoctorData.php
@@ -0,0 +1,23 @@
+<?php
+include "connectToDatabase.php";
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+          // Retrieve the form data
+          $doctorId = $_POST['doctorId'];
+          $fullName = $_POST['fullName'];
+          $phoneNumber = $_POST['phoneNumber'];
+          $speciality = $_POST['speciality'];
+
+          // Insert the data into the database
+          $query = "INSERT INTO doctor (doctorId, fullName, phoneNumber, speciality)
+              VALUES ('$doctorId', '$fullName', '$phoneNumber', '$speciality')";
+
+          if (!mysqli_query($connection, $query)) {
+                    echo "Error: " . mysqli_error($connection);
+          } else {
+                    header('location: doctorData.php');
+          }
+}
+$connection->close();
+?>
