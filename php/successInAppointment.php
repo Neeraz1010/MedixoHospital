cbc8d3ea3d2b5f62bfdca4d80be56efc2463884e
Niraj <bca210634_niraj@achsnepal.edu.np>
2023-07-02 08:11:26 +0545

    Edited some lines

--- php/successInAppointment.php
+++ php/successInAppointment.php
@@ -21,14 +21,67 @@
     // Function to close the popup after 5 seconds and redirect to appointment.php
     function closePopup() {
       setTimeout(function() {
-        window.location.href = '../appointment.php';
+        window.location.href = '../index.php';
       }, 5000);
     }
   </script>
 </head>
 <body onload="closePopup()">
   <div class="success-popup">
-    Appointment successfully scheduled!
+    <h3>
+      Appointment successfully scheduled!
+    </h3>
   </div>
 </body>
 </html>
+<?php
+include 'connectToDatabase.php';
+require_once '../Twilio/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Function to send SMS using Twilio API
+function sendSMS($to, $message)
+{
+  require_once 'config.php';
+
+  // Create a Twilio client
+  $client = new Client($accountSid, $authToken);
+
+  // Send the SMS
+  $client->messages->create(
+    $to,
+    [
+      'from' => $twilioNumber,
+      'body' => $message
+    ]
+  );
+}
+
+$doctorQuery = "SELECT doctorId, fullName FROM doctor";
+$appointmentQuery = "SELECT time, date FROM appointments";
+
+$result = mysqli_query($connection, $doctorQuery);
+$resultAppointment = mysqli_query($connection, $appointmentQuery);
+
+if ($result && $resultAppointment) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    $doctorId = $row['doctorId'];
+    $fullName = $row['fullName'];
+  }
+  while ($row = mysqli_fetch_assoc($resultAppointment)) {
+    $time = $row['time'];
+    $date = $row['date'];
+  }
+  // Send SMS notification
+  $message = "Your Appointment with doctor " . $fullName . " is successfully scheduled at " . "$date" . " and at $time o'clock.";
+  sendSMS('+9779847950672', $message);
+}
+
+
+if (!mysqli_query($connection, $doctorQuery) && !mysqli_query($connection, $appointmentQuery)) {
+  echo "Error: " . mysqli_error($connection);
+}
+
+$connection->close();
+?>
